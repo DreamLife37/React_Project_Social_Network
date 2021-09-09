379de1f0cd77024ed5e49694e517e8298ab25c6b
Andrey <fail4iks@gmail.com>
2021-09-09 07:35:42 +0300

    Header, navbar, profile. 11

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/5/59/Logo-Logo.svg' />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return  <div className='content'>
+       <div><img src='https://union-travel.ru/assets/images/country/thailand/resorts/beach1.jpg'></img>
+    </div>
+    <div>
+      ava+descriprion
+    </div>
+    <div>
+      My Posts
+      <div>New Post</div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
