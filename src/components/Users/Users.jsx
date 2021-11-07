621cbdddffb411aa05eb3fb0bd91c116d92c0c48
Andrey <fail4iks@gmail.com>
2021-11-07 08:34:01 +0300

    Class component. 53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,29 +3,29 @@ import React from "react";
 import styles from "./users.module.css"
 import userPhoto from "../../assets/images/user.png"
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items
-            /* [
-                { id: 1, photoUrl: 'https://art-nto.ru/800/600/https/pbs.twimg.com/profile_images/416574591369498624/LkdCJGob.jpeg', followed: true, fullName: 'Andrey', status: 'Life is is Beautiful', location: { city: 'Ivanovo', country: 'Russia' } },
-                { id: 2, photoUrl: 'https://art-nto.ru/800/600/https/pbs.twimg.com/profile_images/416574591369498624/LkdCJGob.jpeg', followed: false, fullName: 'Andrey', status: 'Life is is Beautiful', location: { city: 'Ivanovo', country: 'Russia' } },
-                { id: 3, photoUrl: 'https://art-nto.ru/800/600/https/pbs.twimg.com/profile_images/416574591369498624/LkdCJGob.jpeg', followed: true, fullName: 'Andrey', status: 'Life is is Beautiful', location: { city: 'Ivanovo', country: 'Russia' } }
-            ] */)
-        })
 
+class Users extends React.Component {
+
+    constructor (props) {
+        super (props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
     }
-    return <div>
+
+    render() {
+        return <div>
         {
-            props.users.map(u => <div key={u.id}>
+            this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                            ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                            : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
                     </div>
                 </span>
                 <span>
@@ -41,5 +41,7 @@ let Users = (props) => {
             </div>)
         }
     </div>
+    }
 }
+
 export default Users;
