59353001f9fdd7d142d40584a82b18cb7a404e4f
Andrey <fail4iks@gmail.com>
2021-11-10 07:50:13 +0300

    Pagination for user page. 55

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USER = 'SET_USER';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     users: [],
+    pageSize: 100,
+    totalUsersCount: 0,
+    currentPage: 3
   };
 
 const usersReducer = (state = initialState, action) => {
@@ -33,8 +38,16 @@ const usersReducer = (state = initialState, action) => {
         }
 
       case SET_USER: {
-        return {...state, users: [...state.users, ...action.users]}
+        return {...state, users: action.users}
       }
+      case SET_CURRENT_PAGE: {
+        return {...state, currentPage: action.currentPage}
+      }
+
+      case SET_TOTAL_USERS_COUNT: {
+        return {...state, totalUsersCount: action.count}
+      }
+      
 
       default:
             return state;
@@ -44,4 +57,6 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userID) => ({type: FOLLOW, userID})
 export const unfollowAC = (userID) => ({type: UNFOLLOW,userID})
 export const setUsersAC = (users) => ({type: SET_USER, users})
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 export default usersReducer;
