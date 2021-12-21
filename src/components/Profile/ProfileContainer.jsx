00dc8bf8d0bde53723f9adc1bcb777f4b7b7e1e9
Andrey <fail4iks@gmail.com>
2021-12-21 08:27:55 +0300

    Status, setState, local state. 71

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -53,5 +53,4 @@ let mapStateToProps = (state) => ({
 
 export default compose(
   connect(mapStateToProps, { getUserProfile }),
-  withRouter,
-  withAuthRedirect)(ProfileContainer);
+  withRouter)(ProfileContainer);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 import userPhoto from '../../../assets/images/user.png'
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
 
@@ -18,7 +19,7 @@ const ProfileInfo = (props) => {
       <div className={s.descriptionBlock}>
          <img src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} /> 
 
-    
+    <ProfileStatus status={'Hello my friends'} />
         {/* <img src={props.profile.photos.large} /> */}
 
         <div>My name: {props.profile.fullName}</div>
